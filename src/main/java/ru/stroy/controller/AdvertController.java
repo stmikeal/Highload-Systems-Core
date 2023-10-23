540a01b2ae599336da6172f73b095e07547bfa17
Mike Steph <timetocook420@gmail.com>
2023-10-24 01:36:38 +0300

    feat: get advert api
    added mapping to get advert api

--- src/main/java/ru/stroy/controller/AdvertController.java
+++ src/main/java/ru/stroy/controller/AdvertController.java
@@ -1,21 +1,35 @@
 package ru.stroy.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.stroy.dto.request.AdvertCreateDto;
+import ru.stroy.entity.datasource.Advert;
+import ru.stroy.repositories.AdvertRepository;
 import ru.stroy.services.AdvertService;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/advert")
 public class AdvertController {
     private final AdvertService advertService;
+    private final AdvertRepository advertRepository;
 
     @PutMapping
     public void createAdvert(@RequestBody AdvertCreateDto advertCreateDto) {
         advertService.createAdvertByDto(advertCreateDto);
+    }
+
+    @GetMapping
+    @ResponseBody
+    public List<Advert> getAllAdvert() {
+        return advertRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public Advert getAdvert(@PathVariable Long id) {
+        return advertRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Advert not found"));
     }
 }

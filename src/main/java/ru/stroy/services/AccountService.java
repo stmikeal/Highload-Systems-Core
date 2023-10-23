1f18749b02fde453c54dd3433e00f1e49c5dbb76
Mike Steph <timetocook420@gmail.com>
2023-10-23 17:34:32 +0300

    fix: moved functionality to service
    extended service to move functionality

--- src/main/java/ru/stroy/services/AccountService.java
+++ src/main/java/ru/stroy/services/AccountService.java
@@ -1,18 +1,43 @@
 package ru.stroy.services;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import ru.stroy.dto.request.AccountUpdateDto;
 import ru.stroy.entity.datasource.Account;
+import ru.stroy.entity.datasource.Login;
 import ru.stroy.repositories.AccountRepository;
+import ru.stroy.repositories.security.LoginRepository;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class AccountService {
     private final AccountRepository accountRepository;
+    private final LoginRepository loginRepository;
 
     public Account createEmptyAccount() {
         Account account = new Account();
         return accountRepository.save(account);
     }
 
+    public Account updateAccountByDto(AccountUpdateDto accountUpdateDto) {
+        Account account = getContextAccount();
+        account.setName(accountUpdateDto.getName());
+        account.setBirth(accountUpdateDto.getBirth());
+        account.setAvatarUrl(accountUpdateDto.getAvatarUrl());
+        return accountRepository.save(account);
+    }
+
+    public Account getContextAccount() {
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        Optional<Login> login = loginRepository.findByUsername(name);
+        return login.map(Login::getAccount).orElse(new Account());
+    }
+
+    public Account getAccountById(Long id) {
+        Optional<Login> login = loginRepository.findById(id);
+        return login.map(Login::getAccount).orElse(new Account());
+    }
 }

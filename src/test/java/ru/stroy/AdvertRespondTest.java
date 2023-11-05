f587006248e2161e6f44fffa2791733730c40ddd
Mike Steph <timetocook420@gmail.com>
2023-11-06 00:00:54 +0300

    feat: added JUnit tests
    added JUnit tests for Advert, Account and Company

--- src/test/java/ru/stroy/AdvertRespondTest.java
+++ src/test/java/ru/stroy/AdvertRespondTest.java
@@ -2,55 +2,47 @@ package ru.stroy;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import ru.stroy.entity.datasource.Account;
-import ru.stroy.entity.datasource.AdvertRespond;
 import ru.stroy.repositories.AdvertRespondRepository;
 import ru.stroy.services.AccountService;
 import ru.stroy.services.AdvertRespondService;
 
 import java.util.Optional;
 
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static ru.stroy.utils.ResourceContainer.*;
 
 @ExtendWith(MockitoExtension.class)
 public class AdvertRespondTest {
     private static AdvertRespondService advertRespondService;
 
-    @Mock
-    private static AccountService accountService;
-    @Mock
-    private static AdvertRespondRepository advertRespondRepository;
+    private static final AccountService accountService = mock(AccountService.class);
+    private static final AdvertRespondRepository advertRespondRepository = mock(AdvertRespondRepository.class);
 
     @BeforeAll
     static void init() {
-        advertRespondService = new AdvertRespondService(accountService, advertRespondRepository);
+        when(accountService.getContextAccount())
+                .thenReturn(testAccount1);
+        when(advertRespondRepository.findById(testAdvertRespondApplicant1.getId()))
+                .thenReturn(Optional.of(testAdvertRespondApplicant1));
+        when(advertRespondRepository.findById(testAdvertRespondApplicant2.getId()))
+                .thenReturn(Optional.of(testAdvertRespondApplicant2));
     }
+
+    @BeforeEach
+    void refresh() { advertRespondService = new AdvertRespondService(accountService, advertRespondRepository); }
 
     @Test
     public void checkPositiveAuthorities() {
-        Account account = new Account();
-        account.setId(321L);
-        when(accountService.getContextAccount()).thenReturn(account);
-        AdvertRespond advertRespond = new AdvertRespond();
-        advertRespond.setApplicant(account);
-        when(advertRespondRepository.findById(777L)).thenReturn(Optional.of(advertRespond));
-        Assertions.assertTrue(advertRespondService.isOwnedRespond(777L));
+        Assertions.assertTrue(advertRespondService.isOwnedRespond(testAdvertRespondApplicant1.getId()));
     }
 
     @Test
     public void checkNegativeAuthorities() {
-        Account account = new Account();
-        account.setId(321L);
-        Account account2 = new Account();
-        account2.setId(123L);
-        when(accountService.getContextAccount()).thenReturn(account);
-        AdvertRespond advertRespond = new AdvertRespond();
-        advertRespond.setApplicant(account2);
-        when(advertRespondRepository.findById(777L)).thenReturn(Optional.of(advertRespond));
-        Assertions.assertFalse(advertRespondService.isOwnedRespond(777L));
+        Assertions.assertFalse(advertRespondService.isOwnedRespond(testAdvertRespondApplicant2.getId()));
     }
 }

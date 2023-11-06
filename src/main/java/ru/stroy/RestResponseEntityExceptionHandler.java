b6be5fb4a777e141a31cb643c99224d022cffb68
Mike Steph <timetocook420@gmail.com>
2023-11-06 19:30:05 +0300

    fix: added exception message to response
    Added moving message from exception to response

---
+++ src/main/java/ru/stroy/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,18 @@
+package ru.stroy;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler
+        extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(value = { RuntimeException.class })
+    protected ResponseEntity<?> handleConflict(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+}

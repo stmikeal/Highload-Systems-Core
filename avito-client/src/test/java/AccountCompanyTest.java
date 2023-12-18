998a6beb3209f8ba50a5f9c23e2f6300fe3388f9
buffer404 <lsv141530@gmail.com>
2023-12-18 19:42:57 +0300

    feat: added junit test AccountCompanyTest
    Create AccountCompanyTest

--- avito-client/src/test/java/AccountCompanyTest.java
+++ avito-client/src/test/java/AccountCompanyTest.java
@@ -3,68 +3,48 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
-import ru.stroy.repositories.AreaRepository;
-import ru.stroy.repositories.CompanyRepository;
+import ru.stroy.dto.enumeration.AccessCompanyEnum;
+import ru.stroy.repositories.AccessRepository;
+import ru.stroy.repositories.AccountCompanyLinkRepository;
 import ru.stroy.services.AccountCompanyLinkService;
-import ru.stroy.services.AccountService;
-import ru.stroy.services.CompanyService;
-import ru.stroy.services.TariffService;
-
-import org.springframework.security.access.AccessDeniedException;
-import java.util.Optional;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static util.ResourceContainer.*;
 
 @ExtendWith(MockitoExtension.class)
-@RunWith(MockitoJUnitRunner.class)
 public class AccountCompanyTest {
-    private static CompanyService companyService;
+    private static AccountCompanyLinkService accountCompanyLinkService;
 
-    private static final AccountCompanyLinkService accountCompanyLinkService = mock(AccountCompanyLinkService.class);
-    private static final CompanyRepository companyRepository = mock(CompanyRepository.class);
-    private static final AccountService accountService = mock(AccountService.class);
-    private static final AreaRepository areaRepository = mock(AreaRepository.class);
-    private static final TariffService tariffService = mock(TariffService.class);
+    private static final AccountCompanyLinkRepository accountCompanyLinkRepository = mock(AccountCompanyLinkRepository.class);
+    private static final AccessRepository accessRepository = mock(AccessRepository.class);
 
     @BeforeAll
     static void init() {
-        when(companyRepository.findById(testCompany1.getId()))
-                .thenReturn(Optional.of(testCompany1));
-        when(companyRepository.findById(testCompany2.getId()))
-                .thenReturn(Optional.of(testCompany2));
-        when(accountCompanyLinkService.hasEditPermission(testCompany1.getId(), testAccount1.getId()))
-                .thenReturn(true);
-        when(accountService.getContextAccount()).thenReturn(testAccount1);
+        when(accountCompanyLinkRepository.existsAccountCompanyLinkByAccount_IdAndCompany_IdAndAccess_Code(
+                testAccount1.getId(), testCompany1.getId(), AccessCompanyEnum.Owner.getCode()
+        )).thenReturn(true);
+        when(accountCompanyLinkRepository.existsAccountCompanyLinkByAccount_IdAndCompany_IdAndAccess_Code(
+                testAccount2.getId(), testCompany1.getId(), AccessCompanyEnum.Moderator.getCode()
+        )).thenReturn(true);
     }
 
     @BeforeEach
-    void refresh() {
-        companyService = new CompanyService(
-                accountCompanyLinkService,
-                companyRepository,
-                accountService,
-                areaRepository,
-                tariffService
-        );
+    void refresh() { accountCompanyLinkService = new AccountCompanyLinkService(accountCompanyLinkRepository, accessRepository); }
+
+    @Test
+    public void checkPermissionOfCompanyOwner() {
+        Assertions.assertTrue(accountCompanyLinkService.hasEditPermission(testAccount1.getId(), testCompany1.getId()));
     }
 
     @Test
-    public void checkEditPermissionOnAdmin() {
-        Assertions.assertEquals(testCompany1, companyService.getCompanyWithPermission(testCompany1.getId()));
+    public void checkPermissionOfCompanyModerator() {
+        Assertions.assertTrue(accountCompanyLinkService.hasEditPermission(testAccount2.getId(), testCompany1.getId()));
     }
 
     @Test
-    public void checkEditPermissionOnUnknownCompany() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> companyService.getCompanyWithPermission(testCompany1.getId() + 1));
-    }
-
-    @Test
-    public void checkEditPermissionOnNotPermitted() {
-        Assertions.assertThrows(AccessDeniedException.class, () -> companyService.getCompanyWithPermission(testCompany2.getId()));
+    public void checkPermissionOfCompanyNotPermittedPersons() {
+        Assertions.assertFalse(accountCompanyLinkService.hasEditPermission(testAccount3.getId(), testCompany1.getId()));
     }
 }

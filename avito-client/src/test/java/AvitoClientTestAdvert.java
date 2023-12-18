7036237bf2f481214c6506310bebd03fcc32e0a6
buffer404 <lsv141530@gmail.com>
2023-12-18 19:14:04 +0300

    feat: added junit test AdvertRespond
    Create AvitoClientTestAdvert.java class and ResourceContainer.java

---
+++ avito-client/src/test/java/AvitoClientTestAdvert.java
@@ -0,0 +1,49 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.stroy.repositories.AdvertRespondRepository;
+import ru.stroy.services.AccountService;
+import ru.stroy.services.AdvertRespondService;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static util.ResourceContainer.*;
+
+
+
+@ExtendWith(MockitoExtension.class)
+public class AvitoClientTestAdvert {
+    private static AdvertRespondService advertRespondService;
+
+
+    private static final AccountService accountService = mock(AccountService.class);
+    private static final AdvertRespondRepository advertRespondRepository = mock(AdvertRespondRepository.class);
+
+    @BeforeAll
+    static void init() {
+        when(accountService.getContextAccount())
+                .thenReturn(testAccount1);
+        when(advertRespondRepository.findById(testAdvertRespondApplicant1.getId()))
+                .thenReturn(Optional.of(testAdvertRespondApplicant1));
+        when(advertRespondRepository.findById(testAdvertRespondApplicant2.getId()))
+                .thenReturn(Optional.of(testAdvertRespondApplicant2));
+    }
+
+    @BeforeEach
+    void refresh() {advertRespondService = new AdvertRespondService(advertRespondRepository, accountService);}
+
+    @Test
+    public void checkPositiveAuthorities() {
+        Assertions.assertTrue(advertRespondService.isOwnedRespond(testAdvertRespondApplicant1.getId()));
+    }
+
+    @Test
+    public void checkNegativeAuthorities() {
+        Assertions.assertFalse(advertRespondService.isOwnedRespond(testAdvertRespondApplicant2.getId()));
+    }
+}
---
+++ avito-client/src/test/java/util/ResourceContainer.java
@@ -0,0 +1,48 @@
+package util;
+
+import ru.stroy.entity.datasource.Account;
+import ru.stroy.entity.datasource.AdvertRespond;
+import ru.stroy.entity.datasource.Company;
+import ru.stroy.entity.datasource.Login;
+
+public class ResourceContainer {
+    public static final String testRandomString1 = "dfkASJ21a_1ASdl";
+
+    public static final Login testLogin1 = createLogin(57875L, "password");
+
+    public static final Account testAccount1 = createAccount(123L);
+    public static final Account testAccount2 = createAccount(321L);
+    public static final Account testAccount3 = createAccount(767L);
+
+    public static final AdvertRespond testAdvertRespondApplicant1 = createRespondWithApplicant(testAccount1, 654L);
+    public static final AdvertRespond testAdvertRespondApplicant2 = createRespondWithApplicant(testAccount2, 4312L);
+
+    public static final Company testCompany1 = createCompany(847L);
+    public static final Company testCompany2 = createCompany(331L);
+
+    private static Login createLogin(Long id, String password) {
+        Login login = new Login();
+        login.setId(id);
+        login.setPassword(password);
+        return login;
+    }
+
+    private static Account createAccount(Long id) {
+        Account account = new Account();
+        account.setId(id);
+        return account;
+    }
+
+    private static AdvertRespond createRespondWithApplicant(Account account, Long id) {
+        AdvertRespond advertRespond = new AdvertRespond();
+        advertRespond.setId(id);
+        advertRespond.setApplicant(account);
+        return advertRespond;
+    }
+
+    private static Company createCompany(Long id) {
+        Company company = new Company();
+        company.setId(id);
+        return company;
+    }
+}
